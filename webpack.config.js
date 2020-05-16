e19b91a6d850d7ab0cfcc8a5464be8c4d147b6b8
Vincent Tran <53809810+vincenttrannz@users.noreply.github.com>
2020-05-16 20:59:38 +1200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, 'client', 'index.js'),
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    }]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new Dotenv()
+  ],
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './server/public'
+  }
+}
